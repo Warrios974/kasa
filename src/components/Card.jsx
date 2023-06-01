f704703a34663e6aaf15ea3d4b1ce2e787fca991
Kenny Lauret <kenny.lauret@outlook.fr>
2023-06-01 13:20:15 +0400

    Modif degradé card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,7 +23,7 @@ const StyledImgFilter = styled.div`
   top: 0;
   width: 100%;
   height: 100%;background: rgb(0,0,0);
-background: linear-gradient(0deg, rgba(0,0,0,0.7) 0%, rgba(255,255,255,0.3) 100%);
+  background: linear-gradient(0deg, rgba(0,0,0,0.7) 0%, rgba(255,255,255,0.3) 100%);
 `
 const StyledTitle = styled.h3`
   position: absolute;
