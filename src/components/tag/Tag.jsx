b36e9f7044691b08782e5f1e68950e2b979df1e5
Kenny Lauret <kenny.lauret@outlook.fr>
2023-06-09 14:07:26 +0400

    Déplacement de la section tag dans tag

--- src/components/tag/Tag.jsx
+++ src/components/tag/Tag.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import style from './Tag.module.css'
 
-function Tag({ tag }) {
+function Tag({ tags }) {
   return (
-    <span className={style.tag}>{tag}</span>
+    <div className={style.tagContainer}>
+        {
+            tags.map((tag, index) => (
+              <span 
+                key={`${index}-tag`} 
+                className={style.tag}
+                >
+                {tag}
+              </span>
+            ))
+        }
+    </div>
+    
   )
 }
 
