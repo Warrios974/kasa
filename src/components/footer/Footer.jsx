3cda24888213bbeb2be6b1addac34acc8fb14464
Kenny Lauret <kenny.lauret@outlook.fr>
2023-06-19 18:12:54 +0400

    Ajout de lien dans le logo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { ReactComponent as LogoWhite } from "../../assets/logo/logo-white.svg";
 import style from './Footer.module.css'
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className={style.footer}>
+      <Link to='/'>
         <LogoWhite className={style.logo} />
+      </Link>
         <p className={style.text}>© 2020 Kasa. All rights reserved</p>
     </footer>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from '../navbar/Navbar'
 import { ReactComponent as LogoColor } from "../../assets/logo/logo-color.svg";
 import style from './Header.module.css'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className={style.header}>
-        <LogoColor className={style.logo} />
+        <Link to="/">
+          <LogoColor className={style.logo} />
+        </Link>
         <Navbar />
     </header>
   )
