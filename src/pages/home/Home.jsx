53c66d0aaf411d742e505130325fb2a5b0952ca1
Kenny Lauret <kenny.lauret@outlook.fr>
2023-06-23 09:11:31 +0400

    Suppression code inutile

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Banner from '../../components/banner/Banner'
 import Card from '../../components/card/Card'
 import data from '../../data/logement.json'
 import style from './Home.module.css'
 
 function Home() {
-
-  useEffect(() => {
-    const getLogements = async () => {
-      
-    }
-    getLogements()
-  }, [])
-
 
   const propsBanner = {
     title : 'Chez vous, partout et ailleurs',
